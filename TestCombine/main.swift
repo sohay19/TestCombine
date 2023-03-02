b392737b1e54fb3652e486f399ea4e417aadc38f
soha <sa4022@naver.com>
2023-03-02 15:25:03 +0900

    publisher 3개로 증가

--- TestCombine/main.swift
+++ TestCombine/main.swift
@@ -9,20 +9,27 @@ import Foundation
 import Combine
 
 
-private var inputData = ""
+private var publisher:[Future<String, Never>] = []
 
+// publisher 생성
+func inputFuture(msg: String) -> Future<String, Never> {
+    return Future<String, Never> { promise in
+        promise(.success(msg))
+    }
+}
 
-// 데이터 입력
-if let input = readLine() {
-    inputData = input
+// 입력 받기
+for _ in 0...2 {
+    if let input = readLine() {
+        publisher.append(inputFuture(msg: input))
+    }
 }
-// 게시자
-let publisher = Future<String, Never> { promise in
-    promise(.success(inputData))
-}
-// 구독
-let subscriber = publisher.sink { complete in
-    print("결과 = \(complete)")
-} receiveValue: { value in
-    print("receive Value = \(value)")
+
+// 구독 및 발송
+for i in 0...2 {
+    _ = publisher[i].sink { complete in
+        print("결과 = \(complete)")
+    } receiveValue: { value in
+        print("receive Value = \(value)")
+    }
 }
