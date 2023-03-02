0c39323ad5da9d43dc1a34d343ec1685655e56c6
soha <sa4022@naver.com>
2023-03-02 16:13:41 +0900

    Published 프로퍼티 추가 Subject 추가

--- TestCombine/main.swift
+++ TestCombine/main.swift
@@ -9,6 +9,8 @@ import Foundation
 import Combine
 
 
+
+//MARK: - 여러개의 Future Publisher
 private var publisher:[Future<String, Never>] = []
 
 // publisher 생성
@@ -21,15 +23,58 @@ func inputFuture(msg: String) -> Future<String, Never> {
 // 입력 받기
 for _ in 0...2 {
     if let input = readLine() {
-        publisher.append(inputFuture(msg: input))
+        publisher.append(
+            inputFuture(msg: input))
     }
 }
 
-// 구독 및 발송
+// 구독
 for i in 0...2 {
     _ = publisher[i].sink { complete in
         print("결과 = \(complete)")
     } receiveValue: { value in
         print("receive Value = \(value)")
+    }
+}
+
+
+//MARK: - Property Wrapper 사용
+private var cancellables = Set<AnyCancellable>()
+
+class PublisherModel {
+    @Published var msg = ""
+    
+    init(msg: String = "") {
+        self.msg = msg
+    }
+}
+
+// Published 프로퍼티를 가진 객체 생성 및 구독
+var model:PublisherModel?
+for _ in 0...2 {
+    if let input = readLine() {
+        model = PublisherModel(msg: input)
+        model?.$msg.sink { value in
+            print("receiveValue: \(value)")
+        }
+        .store(in: &cancellables)
+    }
+}
+
+// 해제
+for pub in cancellables {
+    pub.cancel()
+}
+
+
+//MARK: - Subject
+let passThroughSubject = PassthroughSubject<String, Never>()
+let subscriber = passThroughSubject.sink { value in
+    print("receiveValue = \(value)")
+}
+
+for _ in 0...2 {
+    if let input = readLine() {
+        passThroughSubject.send(input)
     }
 }
